3d9ac562df8a290bc7c128d6ed8c124ff8e06f11
spektra <sergejperederiev@gmail.com>
2026-05-12 14:50:44 +0300

    Виконані зміни

---
+++ Generals/lesson_01/homework_01.py
@@ -0,0 +1,83 @@
+#%%
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+ print(f"{hello} {world}!")
+
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+bananas = apples * 4
+
+
+# task 05 == виправте назви змінних
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
+
+
+"""
+    # Задачі 07 -10:
+    # Переведіть задачі з книги "Математика, 2 клас"
+    # на мову пітон і виведіть відповідь, так, щоб було
+    # зрозуміло дитині, що навчається в другому класі
+"""
+# task 07
+"""
+У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+Скільки всього дерев посадили в саду?
+"""
+apples = 4
+pears = apples + 5
+plums = apples - 2
+all_trees = apples + pears + plums
+print("Всього дерев у саду:", all_trees)
+# task 08
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+temperature_morning = 5
+temperature_afternoon = temperature_morning - 10
+temperature_evening = temperature_afternoon + 4
+print("Температура надвечір:", temperature_evening)
+# task 09
+"""
+Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
+1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
+Скількі сьогодні дітей у театральному гуртку?
+"""
+boys = 24
+girls = boys // 2
+missing_boys_today = 1
+missing_girls_today = 2
+today_attend = boys + girls - missing_boys_today - missing_girls_today
+print("Сьогодні присутньо дітей у театральному гуртку:", today_attend)
+
+# task 10
+"""
+Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
+а третя - як половина вартості першої та другої разом.
+Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
+"""
+first_book = 8
+second_book = first_book + 2
+third_book = (first_book + second_book) // 2
+cost_all_books = first_book + second_book + third_book
+print("Усі книги по одному примірнику коштують:", cost_all_books, "гривень")
